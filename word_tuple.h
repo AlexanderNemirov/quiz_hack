405e1ce89f7269e41ec9d6e44da7521477fdd217
AlexanderNemirov <alexandernemirov98@gmail.com>
2021-11-16 02:12:41 +0300

    loops -> recursion

--- word_tuple.h
+++ word_tuple.h
@@ -3,23 +3,28 @@
 
 #include<string>
 #include<unordered_map>
+#include<vector>
 
 struct WordTuple{
     WordTuple(std::wstring word, std::unordered_map<wchar_t,unsigned> dict)
-    : word_(word), dict_(dict) {}
+    : word_(word) {
+        vec_.reserve(dict.size());
+        for (auto & letter_key : dict)
+            vec_.push_back(letter_key);
+    }
     WordTuple(const WordTuple & other)
-    : word_(other.word_), dict_(other.dict_) {}
+    : word_(other.word_), vec_(other.vec_) {}
     ~WordTuple() {}
 
-    unsigned size() const { return word_.size(); }
+    unsigned size() const { return vec_.size(); }
     //operators overloading
-    bool operator==(const WordTuple & other) { return dict_ == other.dict_; }
+    bool operator==(const WordTuple & other) { return vec_ == other.vec_; }
     bool operator<(const WordTuple & other) { return word_.size() < other.word_.size(); }
     bool operator!=(const WordTuple & other) { return !(*this == other); }
     WordTuple operator=(const WordTuple & other) {
         if (&other != this) {
             word_ = other.word_;
-            dict_ = other.dict_;
+            vec_ = other.vec_;
         }
         return *this;
     }
@@ -27,26 +32,25 @@ struct WordTuple{
     //get methods
     std::wstring get_word() const { return word_; }
     std::wstring & get_word() { return word_; }
-    std::unordered_map<wchar_t,unsigned> get_dict() const { return dict_; }
-    std::unordered_map<wchar_t,unsigned> & get_dict() { return dict_; }
+    std::vector<std::pair<wchar_t,unsigned>> get_vec() const { return vec_; }
+    std::vector<std::pair<wchar_t,unsigned>> & get_vec() { return vec_; }
 
     private:
     std::wstring word_;
-    std::unordered_map<wchar_t,unsigned> dict_;
+    std::vector<std::pair<wchar_t,unsigned>> vec_;
 
     WordTuple() {}
 };
 
 WordTuple operator+(const WordTuple & left, const WordTuple & right) {
-    WordTuple result{left};
-    std::wstring & word = result.get_word();
-    word = word + right.get_word();
-    std::unordered_map<wchar_t,unsigned> & dict = result.get_dict();
-    for (auto &[letter, num] : right.get_dict()) {
-        if (dict.contains(letter))
-            dict[letter] += num;
+    WordTuple result{right};
+    result.word_ = result.word_ + left.word_;
+    for (auto & let_num : left.vec_) {
+        auto vec_let = std::find_if(result.vec_.begin(), result.vec_.end(), [&](const auto & l) {return l.first == let_num.first;});
+        if (vec_let != result.vec_.end())
+            vec_let->second += let_num.second;
         else
-            dict[letter] = num;
+            result.vec_.push_back(let_num);
     }
     return result;
 }
